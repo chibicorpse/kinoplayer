a90f12891d0acdbb017dda6ca46ed1000c32a4dd
or.avrahamy <or.avrahamy@08c4c4a5-7e62-384b-051c-1dfda3d05099>
2010-08-27 21:16:31 +0000

    Note: We might want to pass events along with some parameters that might affect the action - think how it should be done if at all...

--- trunk/src/com/android/kino/logic/event/EventFactory.java
+++ trunk/src/com/android/kino/logic/event/EventFactory.java
@@ -1,5 +1,6 @@
 package com.android.kino.logic.event;
 
+
 public abstract class EventFactory {
 
     public static InputEvent create(int id) {
@@ -9,6 +10,15 @@ public abstract class EventFactory {
         default:
             return null;
         }
+    }
+
+    /**
+     * TODO(remember) To be used to populate settings window.
+     */
+    public static int[] getAllEventIds() {
+        return new int[] {
+                DoubleTapEvent.ID
+        };
     }
 
 }
