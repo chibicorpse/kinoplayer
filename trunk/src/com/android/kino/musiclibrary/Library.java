44e59f4139d39d699268932d5faa01f309379b04
OpherV <OpherV@08c4c4a5-7e62-384b-051c-1dfda3d05099>
2010-08-28 09:54:08 +0000

    added musiclibrary

--- trunk/src/com/android/kino/musiclibrary/Library.java
+++ trunk/src/com/android/kino/musiclibrary/Library.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.cmc.music.metadata.IMusicMetadata;
 import org.cmc.music.metadata.MusicMetadataSet;
 import org.cmc.music.myid3.MyID3;
+import com.android.kino.logic.MediaProperties;
 
 import android.app.Service;
 import android.content.Intent;
@@ -17,6 +18,9 @@ import com.android.kino.Kino;
 
 public class Library extends Service{
 	Kino kino = null;
+	LibraryDB db = null;
+	
+	final String DBNAME ="MusicLibrary";
 	
 	@Override
 	public IBinder onBind(Intent intent) {						
@@ -27,7 +31,11 @@ public class Library extends Service{
 		if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
 			Log.e("Library","SD card not mounted! media state: "+Environment.getExternalStorageState());
 		}		
-		else {			
+		else {
+			//setup db
+			db = new LibraryDB(this, DBNAME, null, 1);
+			
+			
 			//TODO use kino preferences defined mp3 dir
 			File rootDir = Environment.getExternalStorageDirectory();
 			File mp3dir = new File(rootDir.getAbsoluteFile()+"/mp3");
@@ -66,22 +74,37 @@ public class Library extends Service{
 	}
 	
 	private boolean addFileToLibrary(File file){
-		MusicMetadataSet src_set;
-		IMusicMetadata metadata;
-		try {
-			//read metadata
-			src_set = new MyID3().read(file);
-			metadata = src_set.getSimplified();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}			
+		if (!db.songInDb(file)){
 		
-		Log.d("Library","adding "+metadata.getArtist()+" - "+metadata.getSongTitle());
+		MediaProperties mp3file = new MediaProperties(file.getAbsolutePath());
+
+		
+		db.addSong(mp3file.Filename,				
+				mp3file.Title,
+				mp3file.Album.Artist,
+				mp3file.Album.Title,
+				mp3file.Album.Year,
+				mp3file.TrackNumber,
+				mp3file.Genre,
+				mp3file.Duration,
+				mp3file.BitRate);
+		//Log.d("Library",file.getAbsolutePath()+": SONG IS NOT IN DB");
+		Log.d("Library","added to library: "+file.getAbsolutePath());
+		}
+		else{
+			Log.d("Library",file.getAbsolutePath()+": song is in DB");
+		}
 		
 		return true;
 		
 	}
+			
+	private void purgeLibrary(){		
+		//TODO 	
+	}
+	
+	private void updateLibray(){		
+		//TODO 	
+	}	
 
 }
